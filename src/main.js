28b5e46307d23ac3510db8cb9756760c72ed4ce3
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-30 21:27:24 +0930

    Add animation for hamburger menu on click

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 import './styles/output.css'
 
-console.log('Working');
-
 // Add hamburger button functionality
 const hamburgerBtn = document.querySelector('#hamburger');
 const mainMenu = document.querySelector('nav');
+const lineOne = document.querySelector('#line1');
+const lineTwo = document.querySelector('#line2');
+const lineThree = document.querySelector('#line3');
 
 // Hide/show the navigation menu, and update the hamburger button aria-expanded status
 const toggleNav = () => {
-  mainMenu.classList.toggle('hidden');
+  mainMenu.classList.toggle('left-0');
+  mainMenu.classList.toggle('-left-full');
+
+  // Animate hamburger icon
+  lineTwo.classList.toggle('opacity-0');
+  lineOne.classList.toggle('rotate-45');
+  lineOne.classList.toggle('translate-y-2.5');
+  lineThree.classList.toggle('-rotate-45');
+  lineThree.classList.toggle('-translate-y-2.5')
+
 
   // Toggle aria expanded based on state at time of clicking btn
   if (hamburgerBtn.ariaExpanded === 'true') {    
