ed90551193b40cd42c501a105e1e3e07145f10fc
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-30 20:55:47 +0930

    Add hide/show nav and accessibility nav/menu/btn features

--- src/main.js
+++ src/main.js
@@ -2,3 +2,20 @@ import './styles/output.css'
 
 console.log('Working');
 
+// Add hamburger button functionality
+const hamburgerBtn = document.querySelector('#hamburger');
+const mainMenu = document.querySelector('nav');
+
+// Hide/show the navigation menu, and update the hamburger button aria-expanded status
+const toggleNav = () => {
+  mainMenu.classList.toggle('hidden');
+
+  // Toggle aria expanded based on state at time of clicking btn
+  if (hamburgerBtn.ariaExpanded === 'true') {    
+    hamburgerBtn.ariaExpanded = 'false';
+  } else {
+    hamburgerBtn.ariaExpanded = 'true';
+  }
+}
+
+hamburgerBtn.addEventListener('click', toggleNav);
