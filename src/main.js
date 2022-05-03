19441607cf2ac1d598266d58af82611634117c54
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-05-03 16:44:14 +0930

    Add indicator to scroll down for more

--- src/main.js
+++ src/main.js
@@ -57,3 +57,23 @@ const checkScroll = () => {
 };
 
 // window.addEventListener('scroll', checkScroll);
+
+// Show scroll indicator when full scrolled top
+const scrollIndicator = document.querySelector('#seework');
+
+const checkPosition = () => {
+  if (window.scrollY === 0) {   // user has scrolled all the way up
+    scrollIndicator.classList.add('opacity-100');
+    scrollIndicator.classList.remove('opacity-0');
+    scrollIndicator.classList.remove('bottom-0');
+    scrollIndicator.classList.add('bottom-4');
+  } else {
+    scrollIndicator.classList.remove('opacity-0');
+    scrollIndicator.classList.add('opacity-0');
+    scrollIndicator.classList.remove('opacity-100');
+    scrollIndicator.classList.add('bottom-0');
+    scrollIndicator.classList.remove('bottom-4');
+  }
+};
+
+window.addEventListener('scroll', checkPosition);
